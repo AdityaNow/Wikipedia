ae30a714b4e5c5fab8d699bfb0f0d1b4d7921c8c
AdityaNow <adityamali1208@gmail.com>
2024-01-01 22:46:41 +0530

    menu items

--- cypress/e2e/pageObjects/ToolsPage.js
+++ cypress/e2e/pageObjects/ToolsPage.js
@@ -1,5 +1,5 @@
 export class ToolsPage{
-    
+
     toolsElements={
         toolsDropdown : () => cy.get('#vector-page-tools-dropdown-checkbox'),
         pageTools : () => cy.get('#vector-page-tools'),
@@ -21,7 +21,55 @@ export class ToolsPage{
 
     getAllMenuItems()
     {
-        this.toolsElements.menuItems().click();
+        this.toolsElements.menuItems().then((listMI) => {
+                let currentEvents = "";
+                let Contents = "";
+                let RandomArticle = "";
+                let AboutWikipedia = "";
+                let ContactUs = "";
+                let Donate = "";
+
+                let Help = "";
+                let LearnToEdit = "";
+                let CommunityPortal = "";
+                let RecentChanges = "";
+                let UploadFile = "";
+
+                cy.wrap(listMI).contains('Main page').should('exist').should('be.visible');
+                cy.wrap(listMI).contains('Contribute').should('exist').should('be.visible');
+                cy.readFile('cypress/fixtures/main-menu-cy-data.json').then((data) => {
+                    // Access the value of the "CurrentEvents" key
+                    currentEvents = data.CurrentEvents;
+                    Contents = data.Contents;
+                    RandomArticle = data.RandomArticle;
+                    AboutWikipedia = data.AboutWikipedia;
+                    ContactUs = data.ContactUs;
+                    Donate = data.Donate;
+                
+                cy.wrap(listMI).contains(currentEvents).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(Contents).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(RandomArticle).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(AboutWikipedia).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(ContactUs).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(Donate).should('exist').should('be.visible');
+
+                    Help = data.Help;
+                    LearnToEdit = data.LearnToEdit;
+                    CommunityPortal = data.CommunityPortal;
+                    RecentChanges = data.RecentChanges;
+                    UploadFile = data.UploadFile;
+
+                cy.wrap(listMI).contains(Help).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(LearnToEdit).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(CommunityPortal).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(RecentChanges).should('exist').should('be.visible');
+                cy.wrap(listMI).contains(UploadFile).should('exist').should('be.visible');
+
+                });
+
+
+            }
+        );
     }
 
 }
