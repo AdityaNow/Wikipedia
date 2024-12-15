52aa57dee9f152b91e58d499e5f42f54f9d3c216
AdityaNow <adityamali1208@gmail.com>
2024-12-15 21:25:59 +0530

    code refactor

--- cypress/e2e/pageObjects/CommonFuncPage.js
+++ cypress/e2e/pageObjects/CommonFuncPage.js
@@ -1,22 +1,23 @@
-export class CommonFunctions{
-    
-    elements ={
-        searchInput : () => cy.get('#searchInput'),
-        searchBtn : () => cy.get('.cdx-search-input__end-button')
-    }
+export class CommonFunctions {
+  elements = {
+    searchVector: () => cy.get('[href="/wiki/Special:Search"]'),
+    searchInput: () => cy.get("#searchInput"),
+    searchBtn: () => cy.get(".cdx-search-input__end-button"),
+  };
 
-
-    toSearch(keyword){
+  toSearch(keyword) {
+    this.elements
+      .searchVector()
+      .click()
+      .then(() => {
         this.elements.searchInput().type(keyword);
-        this.elements.searchBtn().click(); 
-    }
-    
-    verifySearchResultUrl(keyword1){
-        cy.url().should('eq', keyword1);
-    }
+      });
+    this.elements.searchBtn().click();
+  }
 
-
+  verifySearchResultUrl(keyword1) {
+    cy.url().should("eq", keyword1);
+  }
 }
 
-
-export const CommonFunc = new CommonFunctions()
+export const CommonFunc = new CommonFunctions();
