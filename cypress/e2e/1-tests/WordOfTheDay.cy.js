60094659c7a1d1f363dfbed814cc0a265b0a7043
AdityaNow <adityamali1208@gmail.com>
2025-01-05 19:59:29 +0530

    refactor

--- cypress/e2e/1-tests/WordOfTheDay.cy.js
+++ cypress/e2e/1-tests/WordOfTheDay.cy.js
@@ -4,21 +4,18 @@ import { LandingPage, VerifyPageLanding } from "../pageObjects/LandingPage";
 import { Tools } from "../pageObjects/ToolsPage";
 import { wordDaily } from "../pageObjects/WordOfTheDayPage";
 
+describe("Wiktionary", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.title().should("eq", "Wikipedia");
+    LandingPage.selectLanguage("English");
+    VerifyPageLanding.englishWikiShouldBeDisplayed();
+  });
 
-describe('Wiktionary', () => 
-{
-    beforeEach(() => {
-        cy.visit('/');
-        cy.title().should('eq', 'Wikipedia');
-        LandingPage.selectLanguage('English');
-        VerifyPageLanding.englishWikiShouldBeDisplayed();
-    });
-
-    it('Word of the day', () =>
-    {
-        Tools.toolsDropdown();
-        Tools.selectAOptioFromToolsDropdown('Wiktionary');
-        cy.wait(500);
-        wordDaily.wordofDayPresent();
-    });    
+  it("Word of the day", () => {
+    Tools.toolsDropdown();
+    Tools.selectAOptioFromToolsDropdown("Wiktionary");
+    cy.wait(500);
+    wordDaily.wordofDayPresent();
+  });
 });
