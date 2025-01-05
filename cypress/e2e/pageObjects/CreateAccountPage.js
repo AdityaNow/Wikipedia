250675935ce17da71bb07b88c991790c5686f7a4
AdityaNow <adityamali1208@gmail.com>
2025-01-05 19:32:22 +0530

    indent refactor code

--- cypress/e2e/pageObjects/CreateAccountPage.js
+++ cypress/e2e/pageObjects/CreateAccountPage.js
@@ -1,44 +1,61 @@
-export class CreateAccountPage{
+export class CreateAccountPage {
+  createAccountgPageElements = {
+    createAccountBtn: () =>
+      cy.get('[data-mw="interface"]').get("span").contains("Create account"),
+    usernameLabel: () => cy.get('[for="wpName2"]').contains("Username"),
+    usernameInput: () =>
+      cy.get('[id="wpName2"][placeholder="Enter your username"]'),
+    usernameError: () => cy.get('[class="cdx-message__content"]'),
+    passwordLabel: () => cy.get('[for="wpPassword2"]').contains("Password"),
+    passwordInput: () =>
+      cy.get('[id="wpPassword2"]', '[placeholder="Enter a password"]'),
+    retypePasswordInput: () =>
+      cy.get('[id="wpRetype"]', '[placeholder="Enter password again"]'),
+  };
 
-    createAccountgPageElements={
-        createAccountBtn : () => cy.get('[data-mw="interface"]').get('span').contains('Create account'),
-        usernameLabel : () => cy.get('[for="wpName2"]').contains('Username'),
-        usernameInput : () => cy.get('[id="wpName2"][placeholder="Enter your username"]'),
-        usernameError : () => cy.get('[class="cdx-message__content"]'),
-        passwordLabel : () => cy.get('[for="wpPassword2"]').contains('Password'),
-        passwordInput : () => cy.get('[id="wpPassword2"]' && '[placeholder="Enter a password"]'),
-        retypePasswordInput : () => cy.get('[id="wpRetype"]' && '[placeholder="Enter password again"]')
+  clickCreateAccountBtn() {
+    this.createAccountgPageElements
+      .createAccountBtn()
+      .should("be.visible")
+      .click();
+  }
 
-    }
+  usernameLabelIsPresent() {
+    this.createAccountgPageElements.usernameLabel().should("be.visible");
+  }
 
-    clickCreateAccountBtn(){
-        this.createAccountgPageElements.createAccountBtn().should('be.visible').click();
-    }
+  enterUsername(userNameInput) {
+    this.createAccountgPageElements
+      .usernameInput()
+      .should("be.enabled")
+      .type(userNameInput);
+  }
 
-    usernameLabelIsPresent(){
-        this.createAccountgPageElements.usernameLabel().should('be.visible');
-    }
+  catchUsernameError() {
+    this.createAccountgPageElements
+      .usernameError()
+      .contains(
+        "Username entered already in use. Please choose a different name."
+      );
+  }
 
-    enterUsername(userNameInput){
-        this.createAccountgPageElements.usernameInput().should('be.enabled').type(userNameInput);
-    }
+  passwordLabelIsPresent() {
+    this.createAccountgPageElements.passwordLabel().should("be.visible");
+  }
 
-    catchUsernameError(){
-        this.createAccountgPageElements.usernameError().contains("Username entered already in use. Please choose a different name.")
-    }
+  enterPassword(passwordInput) {
+    this.createAccountgPageElements
+      .passwordInput()
+      .should("be.enabled")
+      .type(passwordInput);
+  }
 
-    passwordLabelIsPresent(){
-        this.createAccountgPageElements.passwordLabel().should('be.visible');
-    }
-
-    enterPassword(passwordInput){
-        this.createAccountgPageElements.passwordInput().should('be.enabled').type(passwordInput);
-    }
-
-    retypePassword(passwordInput){
-        this.createAccountgPageElements.retypePasswordInput().should('be.enabled').type(passwordInput);
-    }
-  
+  retypePassword(passwordInput) {
+    this.createAccountgPageElements
+      .retypePasswordInput()
+      .should("be.enabled")
+      .type(passwordInput);
+  }
 }
 
-export const CreateAccount = new CreateAccountPage()
+export const CreateAccount = new CreateAccountPage();
